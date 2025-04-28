b8506b2ae150f2b491f85fe1f8de482a24e56e30
aouad <aouadmouhamed1@gmail.com>
2025-04-28 22:11:47 +0100

    add the logic of register component

--- FRENT_END/Dentist_frentEnd/src/pages/register.jsx
+++ FRENT_END/Dentist_frentEnd/src/pages/register.jsx
@@ -1,10 +1,61 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function Register() {
+
+  const [form , setForm] = useState({
+      name : '' ,
+      email : '' ,
+      password : '',
+      confirmpass : ''
+  }) ;
+
+
+  const handleChange = (e) =>{
+
+    setForm({
+      ...form ,
+      [e.target.name]: e.target.value,
+    }) ;
+    
+  }
+
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault() ;
+
+    try{
+
+      const res = await axios.post('http://localhost:8000/api/register' , form) ;
+      alert('you registred successful') ;
+      console.log(res.data)
+      localStorage.setItem('token' , res.data.token) ;
+    }
+    catch(e){
+      console.error(e)
+    }
+
+  }
+
   return (
-    <div>
-      register page
-    </div>
+
+    <form onSubmit={handleSubmit}>
+
+      <label htmlFor="">Your Name :</label>
+      <input type="text"  name='name' value={form.name}  onChange={handleChange}/><br />
+
+      <label htmlFor="">Your Email :</label>
+      <input type="email" name='email' value={form.email}  onChange={handleChange}/><br />
+
+      <label htmlFor="">Enter Password</label>
+      <input type="password" name='password' value={form.password}  onChange={handleChange}/><br />
+
+      <label htmlFor="">Confirmation of password</label>
+      <input type="password" name='confirmpass' value={form.confirmpass}  onChange={handleChange}/><br />
+
+      <button> Resister </button>
+   
+    </form>
   )
 }
