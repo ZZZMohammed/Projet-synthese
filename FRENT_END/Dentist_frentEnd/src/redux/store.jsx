4e5f183f25cb0926d4e3a8126578dbae0eff97a1
aouad <aouadmouhamed1@gmail.com>
2025-05-04 22:08:46 +0100

    add reducers

--- FRENT_END/Dentist_frentEnd/src/redux/store.jsx
+++ FRENT_END/Dentist_frentEnd/src/redux/store.jsx
@@ -3,6 +3,7 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunk from 'redux-thunk'
 
+
 const rootReducer = combineReducers({
     timeSlots: timeSlotReducer,
     appointments: appointmentReducer,
