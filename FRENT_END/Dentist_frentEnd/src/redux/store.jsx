f50dc2bc65de055552af08a107b17d0578c9b63e
aouad <aouadmouhamed1@gmail.com>
2025-05-05 01:35:55 +0100

    debuggin

--- FRENT_END/Dentist_frentEnd/src/redux/store.jsx
+++ FRENT_END/Dentist_frentEnd/src/redux/store.jsx
@@ -1,9 +1,11 @@
 
 
 import {applyMiddleware, combineReducers, createStore} from 'redux'
-import thunk from 'redux-thunk'
+import { thunk } from 'redux-thunk';
 import { timeSlotReducer } from './reducers/timeSlotReducer'
 import {appointmentReducer} from './reducers/appointmentReducer'
+
+
 
 const rootReducer = combineReducers({
     timeSlots: timeSlotReducer,
