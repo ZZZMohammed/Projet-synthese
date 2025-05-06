e20bd7664359cf8a2ae0b421a464062519aa3977
aouad <aouadmouhamed1@gmail.com>
2025-05-06 17:11:04 +0100

    debugg and test

--- FRENT_END/Dentist_frentEnd/src/redux/actions/timeSlotAction.js
+++ FRENT_END/Dentist_frentEnd/src/redux/actions/timeSlotAction.js
@@ -4,18 +4,11 @@ export const fetchTimeSlots = () => async (dispatch) => {
     try {
         dispatch({ type: 'TIMESLOT_REQUEST' });
 
-        // Retrieve the token from localStorage
-        const token = localStorage.getItem('token');
-
-        if (!token) {
-            throw new Error('Authentication token is missing.');
-        }
+       
 
         // Make the GET request with the token in the Authorization header
         const res = await axios.get('http://localhost:8000/api/times', {
-            headers: {
-                Authorization: `Bearer ${token}`, // Add the token here
-            },
+           
         });
 
         dispatch({ type: 'TIMESLOT_SUCCESS', payload: res.data }); // Dispatch success with the data
