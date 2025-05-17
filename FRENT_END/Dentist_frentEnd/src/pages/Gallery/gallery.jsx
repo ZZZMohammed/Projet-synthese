7eb46f954ed08b0c0766cb77c7bbd9cb48531f30
aouad <aouadmouhamed1@gmail.com>
2025-05-17 09:50:55 +0100

    style with bootstrap

--- FRENT_END/Dentist_frentEnd/src/pages/Gallery/gallery.jsx
+++ FRENT_END/Dentist_frentEnd/src/pages/Gallery/gallery.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 import '../Gallery/galler.css'
 
 export default function Gallery() {
+
+  const images = [
+      {id:1 , src:'img/g1.png'},
+       {id:2 , src:'img/g2.jpg'},
+        {id:3 , src:'img/g3.jpg'},
+         {id:4 , src:'img/g4.jpg'},
+          {id:5 , src:'img/g5.jpg'},
+           {id:6 , src:'img/g6.jpg'}
+           
+  ] ;
+
+
   return (
     <>
     
@@ -12,8 +24,17 @@ export default function Gallery() {
           
     </div>
 
-    <div className=''>
-      
+    <div className='container'>
+        <div className="row justify-content-center">
+            {images.map((image)=> (
+                <div key={image.id} className='col-md-3 col-sm-6 mb-4 text-center'>
+                    <img src={image.src}
+                       className="img-fluid gallery-image"/>
+                </div>
+            )
+
+            )}
+        </div>
     </div>
     </>
 
